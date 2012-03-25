e5b63c59a8bf436b6f9bd8199cbef9df2fc76ae6
Mikael Emilsson <mikael.emilsson@gmail.com>
2012-03-25 13:08:56 +0200

    lol

--- tmdb.js
+++ tmdb.js
@@ -9,38 +9,39 @@ String.prototype.format = function() {
     return content;
 };
 
-var api_urls =
-    {
-        base: 'http://api.themoviedb.org/3',
-        configuration: '{0}/configuration?api_key={1}',
-        misc_latest: '{0}/latest/movie?api_key={1}',
-        misc_now_playing: '{0}/movie/now-playing?api_key={1}&page={2}',
-        misc_popular: '{0}/movie/popular?api_key={1}&page={2}',
-        misc_top_rated: '{0}/movie/top-rated?api_key={1}&page={2}',
-        movie_info: '{0}/movie/{1}?api_key={2}',
-        movie_alternative_titles: '{0}/alternative_titles?api_key={2}',
-        movie_casts: '{0}/movie/{1}/casts?api_key={2}',
-        movie_images: '{0}/movie/{1}/images?api_key={2}',
-        movie_keywords: '{0}/movie/{1}/keywords?api_key={2}',
-        movie_releases: '{0}/movie/{1}/releases?api_key={2}',
-        movie_trailers: '{0}/movie/{1}/trailers?api_key={2}',
-        movie_translations: '{0}/movie/{1}/translations?api_key={2}',
-        person_info: '{0}/person/{1}?api_key={2}',
-        person_credits: '{0}/person/{1}/credits?api_key={2}',
-        person_images: '{0}/person/{1}/images?api_key={2}',
-        collection_info: '{0}/collection/{1}?api_key={2}',
-        search_movie: '{0}/search/movie?query={1}&api_key={2}&page={3}',
-        search_person: '{0}/search/person?query={1}&api_key={2}&page={3}'
-    };
-
-var apikey;
-var config;
-
 module.exports.init = function(apikey) {
     this.apikey = apikey;
     this.config = null;
     return this;
 }
+
+var apikey;
+var config;
+
+var base = 'http://api.themoviedb.org/3'; 
+var api_urls =
+    {
+        configuration:              base+'/configuration?api_key={0}',
+        misc_latest:                base+'/latest/movie?api_key={0}',
+        misc_now_playing:           base+'/movie/now-playing?api_key={0}&page={1}',
+        misc_popular:               base+'/movie/popular?api_key={0}&page={1}',
+        misc_top_rated:             base+'/movie/top-rated?api_key={0}&page={1}',
+        movie_info:                 base+'/movie/{0}?api_key={1}',
+        movie_alternative_titles:   base+'/movie/{0}/alternative_titles?api_key={1}',
+        movie_casts:                base+'/movie/{0}/casts?api_key={1}',
+        movie_images:               base+'/movie/{0}/images?api_key={1}',
+        movie_keywords:             base+'/movie/{0}/keywords?api_key={1}',
+        movie_releases:             base+'/movie/{0}/releases?api_key={1}',
+        movie_trailers:             base+'/movie/{0}/trailers?api_key={1}',
+        movie_translations:         base+'/movie/{0}/translations?api_key={1}',
+        person_info:                base+'/person/{0}?api_key={1}',
+        person_credits:             base+'/person/{0}/credits?api_key={1}',
+        person_images:              base+'/person/{0}/images?api_key={1}',
+        collection_info:            base+'/collection/{0}?api_key={1}',
+        search_movie:               base+'/search/movie?query={0}&api_key={1}&page={2}',
+        search_person:              base+'/search/person?query={0}&api_key={1}&page={2}'
+    };
+
 
 module.exports.read_config = function(res) {
     this.config = res;
@@ -52,22 +53,22 @@ module.exports.read_config = function(res) {
  **/
 module.exports.Misc = {
     latest: function(callback) {
-        var url = api_urls['misc_latest'].format(api_urls['base'], exports.apikey);
+        var url = api_urls['misc_latest'].format(exports.apikey);
         exports.fetchexternal({url: url}, callback);
     },
     now_playing: function(p, callback) {
         var page = ((typeof p !== "number") ? page = 1 : page = p);
-        var url = api_urls['misc_now_playing'].format(api_urls['base'], exports.apikey, page);
+        var url = api_urls['misc_now_playing'].format(exports.apikey, page);
         exports.fetchexternal({url: url}, callback);
     },
     popular: function(p, callback) {
         var page = ((typeof p !== "number") ? page = 1 : page = p);
-        var url = api_urls['misc_popular'].format(api_urls['base'], exports.apikey, page);
+        var url = api_urls['misc_popular'].format(exports.apikey, page);
         exports.fetchexternal({url: url}, callback);
     },
     top_rated: function(p, callback) {
         var page = ((typeof p !== "number") ? page = 1 : page = p);
-        var url = api_urls['misc_top_rated'].format(api_urls['base'], exports.apikey, page);
+        var url = api_urls['misc_top_rated'].format(exports.apikey, page);
         exports.fetchexternal({url: url}, callback);
     }
 };
@@ -77,7 +78,7 @@ module.exports.Misc = {
  **/
 module.exports.Configuration =
     function(callback) {
-        var url = api_urls['configuration'].format(api_urls['base'], exports.apikey);
+        var url = api_urls['configuration'].format(exports.apikey);
         exports.fetchexternal({url: url}, callback);
     };
 
@@ -87,35 +88,35 @@ module.exports.Configuration =
  **/
 module.exports.Movie = {
     info: function(q, callback) {
-        var url = api_urls['movie_info'].format(api_urls['base'], q, exports.apikey);
+        var url = api_urls['movie_info'].format(q, exports.apikey);
         exports.fetchexternal({url:url}, callback);
     },
     alternative_titles: function(q, callback) {
-        var url = api_urls['movie_alternative_titles'].format(api_urls['base'], q, exports.apikey);
+        var url = api_urls['movie_alternative_titles'].format(q, exports.apikey);
         exports.fetchexternal({url:url}, callback);
     },
     casts: function(q, callback) {
-        var url = api_urls['movie_casts'].format(api_urls['base'], q, exports.apikey);
+        var url = api_urls['movie_casts'].format(q, exports.apikey);
         exports.fetchexternal({url:url}, callback);
     },
     images: function(q, callback) {
-        var url = api_urls['movie_images'].format(api_urls['base'], q, exports.apikey);
+        var url = api_urls['movie_images'].format(q, exports.apikey);
         exports.fetchexternal({url:url}, callback);
     },
     keywords: function(q, callback) {
-        var url = api_urls['movie_keywords'].format(api_urls['base'], q, exports.apikey);
+        var url = api_urls['movie_keywords'].format(q, exports.apikey);
         exports.fetchexternal({url:url}, callback);
     },
     releases: function(q, callback) {
-        var url = api_urls['movie_releases'].format(api_urls['base'], q, exports.apikey);
+        var url = api_urls['movie_releases'].format(q, exports.apikey);
         exports.fetchexternal({url:url}, callback);
     },
     trailers: function(q, callback) {
-        var url = api_urls['movie_trailers'].format(api_urls['base'], q, exports.apikey);
+        var url = api_urls['movie_trailers'].format(q, exports.apikey);
         exports.fetchexternal({url:url}, callback);
     },
     translations: function(q, callback) {
-        var url = api_urls['movie_translations'].format(api_urls['base'], q, exports.apikey);
+        var url = api_urls['movie_translations'].format(q, exports.apikey);
         exports.fetchexternal({url:url}, callback);
     },
 };
@@ -128,13 +129,13 @@ module.exports.Movie = {
 module.exports.Search = {
     movie: function(q, callback) {
         var page = ((typeof q.page !== "number") ? page = 1 : page = q.page);
-        var url = api_urls['search_movie'].format(api_urls['base'], q.query, exports.apikey, page);
+        var url = api_urls['search_movie'].format(q.query, exports.apikey, page);
         console.log(url);
         exports.fetchexternal({url:url}, callback);
     },
     person: function(q, callback) {
         var page = ((typeof q.page !== "number") ? page = 1 : page = q.page);
-        var url = api_urls['search_person'].format(api_urls['base'], q.query, exports.apikey, page);
+        var url = api_urls['search_person'].format(q.query, exports.apikey, page);
         exports.fetchexternal({url:url}, callback);
     }
 };
@@ -145,15 +146,15 @@ module.exports.Search = {
  **/
 module.exports.Person = {
     info: function(q, callback) {
-        var url = api_urls['person_info'].format(api_urls['base'], q, exports.apikey);
+        var url = api_urls['person_info'].format(q, exports.apikey);
         exports.fetchexternal({url:url}, callback);
     },
     credits: function(q, callback) {
-        var url = api_urls['person_credits'].format(api_urls['base'], q, exports.apikey);
+        var url = api_urls['person_credits'].format(q, exports.apikey);
         exports.fetchexternal({url:url}, callback);       
     },
     images: function(q, callback) {
-        var url = api_urls['person_images'].format(api_urls['base'], q, exports.apikey);
+        var url = api_urls['person_images'].format(q, exports.apikey);
         exports.fetchexternal({url:url}, callback);       
     }
 };
@@ -164,7 +165,7 @@ module.exports.Person = {
  **/
 module.exports.Collection = {
     info: function(q, callback) {
-        var url = api_urls['collection_info'].format(api_urls['base'], q, exports.apikey);
+        var url = api_urls['collection_info'].format(q, exports.apikey);
         exports.fetchexternal({url: url}, callback);
     }
 };
